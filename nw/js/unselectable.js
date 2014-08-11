996f01073cb9f9588bcb528086982bc1242e97ac
LiangGuanyu <lianggy0719@126.com>
2014-08-11 19:40:39 +0800

    use less to generate css

--- nw/js/unselectable.js
+++ nw/js/unselectable.js
@@ -2,10 +2,14 @@ var BASE_APP_PATH = "/usr/share/applications/";
 var gedit = undefined;
 var terminal = undefined;
 
-$(document).ready(function() {
+function loadDEntries() {
 	gedit = new DEntry('gedit');
 	terminal = new DEntry('terminal');
 
 	gedit.show();
 	terminal.show();
+}
+
+$(document).ready(function() {
+	loadDEntries();
 });
