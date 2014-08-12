e7dfb1169caafdb368e0a4e72148fb337949feb1
LiangGuanyu <lianggy0719@126.com>
2014-08-12 15:30:44 +0800

    add a grid in desktop

--- nw/js/dentry.js
+++ nw/js/dentry.js
@@ -1,3 +1,4 @@
+//base class for varies dentries
 //id_: DEntry's Unic ID.
 //position_({left, top}): The position of this DEntry.
 //path_: file path
@@ -18,15 +19,20 @@ function DEntry(id_, position_, path_) {
 		//public functions
 		
 		show: function() {
-			dEntry.html(PATTERN);
+			if(typeof position === 'undefined') {
+				alert("no position!!");
+				return ;
+			}
 
-			$('body').append(dEntry);
+			dEntry.html(PATTERN);
+			$('#grid' + position.x + position.y).append(dEntry);
 		},
 
 		getPosition: function() {return position;},
 
 		setPosition: function(position_) {
 			//redraw it with new position
+			//$('#' + id).attr();
 			position = position_;
 		},
 
@@ -39,6 +45,122 @@ function DEntry(id_, position_, path_) {
 		setName: function(name_) {
 			//redraw dentry's name
 			name = name_;
+		}
+	};
+}
+
+//Desktop Entry for application files (a.k.a .desktop)
+//
+function AppEntry(id_, position_, path_) {
+	var super_ = new DEntry(id_, position_, path_);
+
+	return {
+		//inherit functions
+		
+		show: function() {
+			super_.show();
+		},
+
+		getPosition: function() {super_.getPosition;},
+
+		setPosition: function(position_) {
+			super_.setPosition(position_);
+		},
+
+		getID: function() {super_.getID();},
+
+		setID: function(id_) {super_.setID(id_);},//needed?
+
+		getName: function() {super_.getName();},
+
+		setName: function(name_) {
+			super_.setName(name_);
+		},
+
+		//self funtions
+		open: function() {
+			//launch app
+		}
+	};
+}
+
+//Desktop Entry for directories
+//
+function DirEntry(id_, position_, path_) {
+	var super_ = new DEntry(id_, position_, path_);
+
+	return {
+		//inherit functions
+		
+		show: function() {
+			super_.show();
+		},
+
+		getPosition: function() {super_.getPosition;},
+
+		setPosition: function(position_) {
+			super_.setPosition(position_);
+		},
+
+		getID: function() {super_.getID();},
+
+		setID: function(id_) {super_.setID(id_);},//needed?
+
+		getName: function() {super_.getName();},
+
+		setName: function(name_) {
+			super_.setName(name_);
+		},
+
+		//self funtions
+		open: function() {
+			//open dir
+		}
+	};
+}
+
+//Desktop Entry for normal files
+//
+function FileEntry(id_, position_, path_) {
+	var super_ = new DEntry(id_, position_, path_);
+	var type = undefined;
+	
+	function parseType(path__) {
+		//get file type from path__
+		console.log("type is " + type);
+	}
+
+	//constructor
+	(function () {
+		type = parseType(path_);
+	})();
+
+	return {
+		//inherit functions
+		
+		show: function() {
+			super_.show();
+		},
+
+		getPosition: function() {super_.getPosition;},
+
+		setPosition: function(position_) {
+			super_.setPosition(position_);
+		},
+
+		getID: function() {super_.getID();},
+
+		setID: function(id_) {super_.setID(id_);},//needed?
+
+		getName: function() {super_.getName();},
+
+		setName: function(name_) {
+			super_.setName(name_);
+		},
+
+		//self funtions
+		open: function() {
+			//open files with specific app
 		}
 	};
 }
---
+++ nw/js/desktop.js
@@ -0,0 +1,80 @@
+//This class is totally for desktop management
+//
+function Desktop() {
+	var col = 80 + 20;
+	var row = 80 + 10;
+	var col_num = undefined;
+	var row_num = undefined;
+
+	var entries = [];
+	var grid = $('<div>', {
+		'class': 'gridcontainer', 
+		'id': 'grids'
+	});
+
+	var generateGrid = function () {
+		$('body').append(grid);
+		
+		var w = $(document).width();
+		var h = $(document).height();
+		console.log(w + "  " + h);
+		col_num = Math.floor(w * 0.92 / col);
+		row_num = Math.floor(h * 0.9 / row);
+		console.log(col_num + " " + row_num);
+
+		for(var i = 0; i < col_num; ++i) {
+			var col_ = $('<div>', {
+				'class': 'gridcol',
+				'id': 'col' + i
+			});
+			$('#grids').append(col_);
+
+			entries[i] = new Array();
+			for(var j = 0; j < row_num; ++j) {
+				var row_ = $('<div>', {
+					'class': 'grid',
+					'id': 'grid' + i + j
+				});
+				$('#col' + i).append(row_);
+
+				entries[i][j] = {};
+				entries[i][j].use = false;
+			}
+		}
+	};
+
+	var findAnIdleGrid = function() {
+		for(var i = 0; i < col_num; ++i) {
+			for(var j = 0; j < row_num; ++j) {
+				if(entries[i][j].use == false) {
+					return {x: i, y: j};
+				}
+			}
+		}
+		return null;
+	};
+
+	//constructor
+	(function () {
+		generateGrid();
+		console.log("generate!!");
+	})();
+
+	return {
+		loadEntries: function() {},
+
+		addAnDEntry: function(entry_, pos_) {
+			if(typeof pos_ === 'undefined') {
+				pos_ = findAnIdleGrid();
+				if(pos_ == null) {
+					alert("No room");
+					return ;
+				}
+			}
+
+			entry_.setPosition(pos_);
+			entry_.show();
+			entries[pos_.x][pos_.y].use = true;
+		}
+	};
+}
