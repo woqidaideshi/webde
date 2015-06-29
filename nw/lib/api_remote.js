debb254c967fa24d894386e564a3a7bd639e3ec4
LiangGuanyu <lianggy0719@126.com>
2015-06-29 21:56:40 +0800

    visit webde by http

--- nw/lib/api_remote.js
+++ nw/lib/api_remote.js
@@ -1,8 +1,9 @@
 // API lib for remote, this file should be automated from api.js, so don't
 // change this file manually;
 console.log("head of api_remote.js.");
-var WDC = {};
+var WDC= {};
 WDC.isDebug = true;
+WDC.isRemote = true;
 
 /* This function is for require api
  * Param apilist: a string array
@@ -16,5 +17,20 @@ WDC.requireAPI = function(apilist, callback){
   }
   requirejs(apilist, callback);
 }
+
+$(document).ready(function(){
+  if (typeof onStart === "function"){
+    try{
+      var search = window.location.search.substr(1),
+          tmps = search.split('&');
+      WDC.AppID = tmps[0].split('=')[1];
+      var sParam = (typeof tmps[1] === 'undefined') ? 
+            null : tmps[1].replace(/%20/g, ' ').replace(/%22/g, '"').replace(/%27/g, "'");
+      onStart(sParam);
+    }catch(e){
+      console.log("Warning: onStart should be supported :" + e.stack);
+    }
+  }
+});
 
 console.log("end of api_remote.js.");
